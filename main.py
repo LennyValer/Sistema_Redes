f211625e2f651680bc3110ab3a8340de6c04e8ac
LennyValer <107770379+LennyValer@users.noreply.github.com>
2024-09-12 21:06:29 -0500

    creacion de archivos

---
+++ main.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: "))
+
+suma = numero1 + numero2
+
+print("El resultado de la suma es:", suma)
+
+
+
